971df7e9a8d1e44ceabb33679d823516f98ad8d6
Aliffrianto Yudha Prakoso <prakosoyudha56@gmail.com>
2025-07-14 21:03:19 +0700

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true
-  },
-  video: true,
+  
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+      
     },
-    supportFile: 'cypress/support/e2e.js'
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/html',
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+    video: true,
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots'
   },
 });
